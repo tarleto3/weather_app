3b984422302af44958a304b49c3450a21bf2dc48
Caleb Tarleton <ctarleton@gatech.edu>
2019-11-19 18:12:03 -0500

    Removed local testing variables and added start script for launching to Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) => {
     weatherData.textContent = 'Loading...'
     errorDoc.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 errorDoc.textContent = data.error
